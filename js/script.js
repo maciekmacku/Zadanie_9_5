27b9613088522e2249465b86ee00016a7613d6ad
maciekmacku <maciejszalaj@gmail.com>
2018-04-05 20:32:57 +0200

    added buttons into divs, change innerHTML to innerTEXT

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var buttons = document.getElementsByClassName('button');
 for ( var i = 0; i < buttons.length; i++) {
-	alert(buttons[i].innerHTML); // innerTEXT nie dziala
+	alert(buttons[i].innerTEXT); // innerTEXT nie dziala
 }
 console.log(buttons);
